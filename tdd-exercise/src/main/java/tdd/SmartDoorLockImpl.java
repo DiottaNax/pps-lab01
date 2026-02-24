49e66c51350dd1fc59dbbb63bbfd4027be1b521f
Diottanax <fd.diotallevi@gmail.com>
2026-02-24 20:52:45 +0100

    feat: implement base smart door lock implementation

---
+++ tdd-exercise/src/main/java/tdd/SmartDoorLockImpl.java
@@ -0,0 +1,45 @@
+package tdd;
+
+public class SmartDoorLockImpl implements SmartDoorLock {
+    private boolean locked = false;
+
+    @Override
+    public void setPin(int pin) {
+
+    }
+
+    @Override
+    public void unlock(int pin) {
+
+    }
+
+    @Override
+    public void lock() {
+
+    }
+
+    @Override
+    public boolean isLocked() {
+        return this.locked;
+    }
+
+    @Override
+    public boolean isBlocked() {
+        return false;
+    }
+
+    @Override
+    public int getMaxAttempts() {
+        return 0;
+    }
+
+    @Override
+    public int getFailedAttempts() {
+        return 0;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+}
--- tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
@@ -1,12 +1,20 @@
 package tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SmartDoorLockTest {
+    private SmartDoorLock doorLock;
+
+    @BeforeEach
+    void init() {
+        this.doorLock = new SmartDoorLockImpl();
+    }
+
     @Test
-    public void todo() {
-        assertTrue(true);
+    public void shouldBeInitiallyOpen() {
+        assertFalse(this.doorLock.isLocked());
     }
 }
